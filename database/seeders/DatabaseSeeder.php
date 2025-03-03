791d89dadbe0ad150a052cd35abe24b112b9dc50
Ahmed Emam <aemam@trudochealth.com>
2025-03-03 22:31:55 +0400

    add ci / cd for aws

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProjectSeeder::class,
             TimesheetSeeder::class,
+            ProjectUserSeeder::class,
         ]);
         
     }
